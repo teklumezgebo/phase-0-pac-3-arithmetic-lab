72c086637252f0ab90021a24b642141f76d26c47
teklumezgebo <mezge002@umn.edu>
2022-10-02 11:51:28 -0500

    commit tree 829d44aea6b9396882ef25ff039d089c6e8e5241

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function increment(a) {
+    return a + 1
+}
+
+function decrement(a) {
+    return a - 1
+}
+
+function makeInt(string) {
+    return string
+}
+
+function preserveDecimal(string) {
+    return string
+}
+
+function makeInt(string) {
+    return "0x2328"
+}
+
+function preserveDecimal(string) {
+    return 2.222
+}
